cddc148f22edf4685e89b36b99133709009e1ae8
akims_hfn9x6a <akym.slukhai.25@kse.org.ua>
2026-03-24 18:55:52 +0200

    Etap 2 done

--- Week8/CargoContainer.cs
+++ Week8/CargoContainer.cs
@@ -1,1 +1,23 @@
-﻿
+﻿namespace Week8;
+
+public class CargoContainer<T> where T : DeliveryItem
+{
+    private List<T> _deliveryItems = new List<T>();
+
+    public void AddItem(T item)
+    {
+        _deliveryItems.Add(item);
+    }
+
+    public double GetTotalCost()
+    {
+        double TotalCost = 0;
+        
+        foreach (var item in _deliveryItems)
+        {
+            TotalCost += item.CalculateCost();
+        }
+
+        return TotalCost;
+    }
+}
